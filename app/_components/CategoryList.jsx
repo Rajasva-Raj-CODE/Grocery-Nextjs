e40ca348eca0e8623d90d712e26ab4804f05fef6
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-08 23:15:46 +0530

    bg hover

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import Image from "next/image";
 
 const CategoryList = ({ categoryList }) => {
- 
   return (
     <div className="mt-5">
       <h2 className="text-green-600 font-bold text-2xl">Shop By Category</h2>
-      <div>
+      <div className="grid grid-cols-3 md:grid-cols-6 sm:grid-cols-4 lg:grid-cols-7 gap-5 mt-2">
         {categoryList.map((category, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="flex flex-col bg-green-50 gap-2 items-center p-3 rounded-lg group cursor-pointer hover:bg-green-200
+             "
+          >
             <Image
               src={
                 process.env.NEXT_PUBLIC_BACKEND_BASE_URL + category.icon[0]?.url
@@ -17,8 +20,9 @@ const CategoryList = ({ categoryList }) => {
               alt="icon"
               width={50}
               height={50}
+              className="group-hover:scale-125 transition-all ease-in-out duration-300"
             />
-            <h2 className="text-lg">{category.name}</h2>
+            <h2 className="text-green-800">{category.name}</h2>
           </div>
         ))}
       </div>
