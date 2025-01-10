8de22a773f3efc2f61def1fb4139492cc688d787
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-10 20:35:03 +0530

    footer

--- app/page.js
+++ app/page.js
@@ -3,18 +3,31 @@ import Slider from "./_components/Slider";
 import GlobalApi from "./_utils/GlobalApi";
 import CategoryList from "./_components/CategoryList";
 import ProductList from "./_components/ProductList";
+import Image from "next/image";
+import Footer from "./_components/Footer";
 
 const page = async () => {
   const sliderList = await GlobalApi.getSliders();
   const categoryList = await GlobalApi.getCategoryList();
-  const ProductsList= await GlobalApi.getAllProducts();
+  const ProductsList = await GlobalApi.getAllProducts();
 
   return (
     <div className="p-5 md:px-16">
-      <Slider sliderList = {sliderList} />
-      <CategoryList categoryList = {categoryList} />
-      <ProductList ProductsList = {ProductsList} />
+      <Slider sliderList={sliderList} />
+      <CategoryList categoryList={categoryList} />
+      <ProductList ProductsList={ProductsList} />
 
+      {/*Banner*/}
+      <Image
+        src="/banner.png"
+        alt="logo"
+        width={1000}
+        height={300}
+        className="w-full h-[400px] object-contain"
+      />
+
+      {/*Footer*/}
+      <Footer />
     </div>
   );
 };
