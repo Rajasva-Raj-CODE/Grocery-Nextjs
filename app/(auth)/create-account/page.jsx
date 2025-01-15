4b21d3f69207a013f6d1030d74de914f4d80bf0a
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-01-15 23:04:03 +0530

    createAccout

--- app/(auth)/create-account/page.jsx
+++ app/(auth)/create-account/page.jsx
@@ -10,29 +10,29 @@ const CreateAccount = () => {
   const [password, setPassword] = useState("");
   const [Username, setUsername] = useState("");
 
-  const onCreateAccount = () => {
-    
-
-  }
+  const onCreateAccount = () => {};
 
   return (
     <div className="flex items-baseline justify-center my-10">
-      <div className="flex flex-col items-center justify-center gap-5 p-10 bg-slate-100  border-gray-200">
+      <div className="flex flex-col items-center justify-center gap-5 p-10 bg-slate-200  border-gray-200">
         <Image src="/logo.png" alt="logo" width={200} height={200} />
         <h2 className="font-bold text-3xl">Create Account</h2>
         <h2 className="text-gray-500">
           Enter your Email and Password to Create an account
         </h2>
-        <div className="flex flex-col gap-5 mt-7 w-full">
+        <div className="flex flex-col gap-5 mt-7 w-full ">
           <Input
-            placeholder="Username"
+            className="bg-white p-5"
+            placeholder="Username "
             onChange={(e) => setUsername(e.target.value)}
           />
           <Input
+            className="bg-white p-5"
             placeholder="name@example.com"
             onChange={(e) => setEmail(e.target.value)}
           />
           <Input
+            className="bg-white p-5"
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -31,7 +31,7 @@ const getProductsByCategory = (category) =>
     return res.data.data;
   });
 
-  const 
+  
 
 export default {
   getCategory,
